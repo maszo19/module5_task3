d5c7f76b33b36b312c8f9b65bf58ec547d630b22
maszo19 <marcin.szostek@protonmail.com>
2022-02-09 00:54:08 +0100

    Init Business Cards project

---
+++ business-cards.py
@@ -0,0 +1,16 @@
+class BusinessCar:
+    def __init__(self, first_name, last_name, company, position, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.company = company
+        self.position = position
+        self.email = email
+
+
+
+
+
+
+
+
+
