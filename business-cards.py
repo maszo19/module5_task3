f4dace1fe49f3d6c2b2d6037c6bf4807f41456b5
maszo19 <marcin.szostek@protonmail.com>
2022-02-09 02:21:10 +0100

    Create classes BaseContact and its hair BusinessContact

--- business-cards.py
+++ business-cards.py
@@ -1,12 +1,11 @@
 from faker import Faker
 
 
-class BusinessCard:
-    def __init__(self, first_name, last_name, company, position, email):
+class BaseContact:
+    def __init__(self, first_name, last_name, phone, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.company = company
-        self.position = position
+        self.phone = phone
         self.email = email
 
     def __str__(self):
@@ -17,25 +16,46 @@ class BusinessCard:
         return len(self.first_name + " " + self.last_name)
 
     def contact(self):
-        print(f"Contacting {self.first_name} {self.last_name}, the {self.position}: {self.email}")
+        print(f"Dialing {self.phone} to contact {self}")
 
 
-def create_contacts(int):
+class BusinessContact(BaseContact):
+    def __init__(self, company, business_phone, position, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.company = company
+        self.position = position
+        self.business_phone = business_phone
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.position.lower()} in {self.company}, <{self.email}>"
+
+    def contact(self):
+        print(f"Dialing {self.business_phone} to contact {self}")
+
+
+def create_contacts(class_type, int):
     faker = Faker()
     list = []
     for i in range(int):
         name = faker.name()
         fname, lname = name.split(' ')
-        com = faker.company()
-        job = faker.job()
         domain = faker.free_email_domain()
         email = f"{fname.lower()}.{lname.lower()}@{domain}"
-        contact = BusinessCard(first_name=fname, last_name=lname, company=com, position=job.lower(), email=email)
+        phone = faker.phone_number()
+
+        if class_type == "BaseContact":
+            contact = BaseContact(first_name=fname, last_name=lname, phone=phone, email=email)
+        elif class_type == "BusinessContact":
+            com = faker.company()
+            job = faker.job()
+            b_phone = faker.phone_number()
+            contact = BusinessContact(first_name=fname, last_name=lname, phone=phone, email=email, company=com,
+                                      position=job, business_phone=b_phone)
         list.append(contact)
     return list
 
 
-contacts = create_contacts(5)
+contacts = create_contacts("BaseContact", 3) + create_contacts("BusinessContact", 3)
 for card in contacts:
     card.contact()
     print(f"The name is {card.name_len} chars long.")
